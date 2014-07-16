ceb791b6c338b0ff54cf18874ee0fea235e076e8
Bryan Drewery <bryan@shatow.net>
2014-07-15 20:06:12 -0500

    Install .login_conf properly

--- install.sh
+++ install.sh
@@ -52,7 +52,7 @@ ln -fs ${REPO}/dot.gitignore ~/.gitignore
 ln -fs ${REPO}/dot.inputrc ~/.inputrc
 ln -fs ${REPO}/dot.lessfilter ~/.lessfilter
 rm -f ~/.login_conf
-install -v dot.login_conf ~/.login_conf
+install -v ${REPO}/dot.login_conf ~/.login_conf
 ln -fs ${REPO}/dot.nanorc ~/.nanorc
 ln -fs ${REPO}/dot.profile.common ~/.profile.common
 ln -fs ${REPO}/dot.profile.logout ~/.profile.logout
