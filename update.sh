3d58fcd483db5798bbfb08bb64b05a9f424d786f
Bryan Drewery <bryan@shatow.net>
2014-09-05 22:50:28 -0500

    Submodule --depth=1 doesn't work unless the bound hash is the head...

--- update.sh
+++ update.sh
@@ -7,9 +7,9 @@ git reset --hard origin/master
 git reflog expire --expire-unreachable=all --all
 git gc --prune=all
 git submodule init
-if [ $(git version | awk '{print $3}' | cut -d . -f 1) -ge 2 ]; then
-	git submodule update --depth=1
-else
+#if [ $(git version | awk '{print $3}' | cut -d . -f 1) -ge 2 ]; then
+#	git submodule update --depth=1
+#else
 	git submodule update
-fi
+#fi
 exec ./install.sh
