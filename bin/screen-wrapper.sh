5480248165caedd68eb779b868d2f9d08db288ab
Bryan Drewery <bryan@shatow.net>
2015-04-01 18:03:11 -0500

    Show cmd ran in signal handler

--- bin/screen-wrapper.sh
+++ bin/screen-wrapper.sh
@@ -1,16 +1,19 @@
 #! /bin/sh
 # $Id$
 
+_ARGS="$@"
 sigint_handler() {
-        exec ${SHELL}
+	echo
+	echo "# ${_ARGS}"
+	exec ${SHELL}
 }
 [ -n "${SHELL}" ] && trap sigint_handler INT TERM EXIT
 
 export RAN_FROM_CRON=1
-while true; 
-do 
- echo "$@";
- "$@";
- echo "Program terminated..sleeping"
- sleep 10
+while true;
+do
+	echo "# $@";
+	"$@";
+	echo "Program terminated..sleeping"
+	sleep 10
 done
