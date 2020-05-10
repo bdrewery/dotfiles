3c80d82eb39800532aadfdc5f184bdc0d2124a74
Bryan Drewery <bryan@shatow.net>
2020-05-09 20:09:40 -0700

    Avoid parsing errors on update

--- update.sh
+++ update.sh
@@ -1,15 +1,18 @@
 #! /bin/sh
 # $Id$
 
-cd ~/.profile-repo
-git fetch origin --depth=1
-git reset --hard origin/master
-git reflog expire --expire-unreachable=all --all
-git gc --prune=all
-git submodule init
-#if [ $(git version | awk '{print $3}' | cut -d . -f 1) -ge 2 ]; then
-#	git submodule update --depth=1
-#else
+update() {
+	cd ~/.profile-repo
+	git fetch origin --depth=1
+	git reset --hard origin/master
+	git reflog expire --expire-unreachable=all --all
+	git gc --prune=all
+	git submodule init
+	#if [ $(git version | awk '{print $3}' | cut -d . -f 1) -ge 2 ]; then
+	#	git submodule update --depth=1
+	#else
 	git submodule update
-#fi
-exec ./install.sh
+	#fi
+	exec ./install.sh
+}
+update
