dce674f0fed35cc7e23b4436c00714cf87a82b7a
Bryan Drewery <bryan@shatow.net>
2022-06-29 19:42:36 -0700

    screen-wrapper: Sleep up to 60 seconds

--- bin/screen-wrapper.sh
+++ bin/screen-wrapper.sh
@@ -10,10 +10,25 @@ sigint_handler() {
 [ -n "${SHELL}" ] && trap sigint_handler INT TERM EXIT
 
 export RAN_FROM_CRON=1
+cnt=0
+max=60
 while true;
 do
 	echo "# screen-wrapper.sh $@";
 	fixscreen "$@";
+	ret="$?"
 	echo "Program terminated..sleeping"
-	sleep 10
+	if [ "${ret}" -eq 0 ]; then
+		cnt=0
+		continue
+	fi
+	if [ "${cnt}" -lt 5 ]; then
+		sleep 1
+	else
+		sleep "${cnt}"
+	fi
+	cnt=$((cnt + 1))
+	if [ "${cnt}" -gt "${max}" ]; then
+		cnt=0
+	fi
 done
