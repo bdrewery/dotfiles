867cfaf9ae498c056f7d732cd1f0a0717b0e3c6c
Bryan Drewery <bryan@shatow.net>
2022-10-05 07:39:54 -0700

    screen-wrapper: Proceed on input

--- bin/screen-wrapper.sh
+++ bin/screen-wrapper.sh
@@ -10,10 +10,10 @@ sigint_handler() {
 [ -n "${SHELL}" ] && trap sigint_handler INT TERM EXIT
 
 export RAN_FROM_CRON=1
-cnt=0
 max=60
 while true;
 do
+	sleep="${max}"
 	echo "# screen-wrapper.sh $@";
 	fixscreen "$@";
 	ret="$?"
@@ -21,17 +21,9 @@ do
 	case "${ret}" in
 	0|130)
 		# 130 = SIGINT
-		cnt=0
+		sleep=0
 		continue
 		;;
 	esac
-	if [ "${cnt}" -lt 5 ]; then
-		sleep 1
-	else
-		sleep "${cnt}"
-	fi
-	cnt=$((cnt + 1))
-	if [ "${cnt}" -gt "${max}" ]; then
-		cnt=0
-	fi
+	read -t "${sleep}" _ || :
 done
