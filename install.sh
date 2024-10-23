a253bbadd3a15bb48904d6d19f9aeeffb42a5529
Bryan Drewery <bryan@shatow.net>
2024-10-23 08:27:04 -0700

    Alias tf=terraform

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ else
 fi
 
 for f in alert-on-exit.sh benv.sh generate-tags generate-tagsd \
-    screen-wrapper.sh fixscreen make start-screen update-profile \
+    screen-wrapper.sh fixscreen make start-screen tf update-profile \
     lscolors lscolors 24-bit-color.sh; do
 	if [ -L ~/bin/${f} ]; then
 		rm -f ~/bin/${f}
