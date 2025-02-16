3a801a78e7065373698f81d21b8ededc4baff14b
Bryan Drewery <bryan@shatow.net>
2025-02-15 23:35:02 -0800

    update: Use shallow checkout for submodules

--- update.sh
+++ update.sh
@@ -3,15 +3,10 @@
 update() {
 	cd ~/.profile-repo
 	git fetch origin --depth=1
+	git submodule update --init --depth=1
 	git reset --hard origin/master
 	git reflog expire --expire-unreachable=all --all
 	git gc --prune=all
-	git submodule init
-	#if [ $(git version | awk '{print $3}' | cut -d . -f 1) -ge 2 ]; then
-	#	git submodule update --depth=1
-	#else
-	git submodule update
-	#fi
 	exec ./install.sh
 }
 update
