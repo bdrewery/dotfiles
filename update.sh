8b3908447bd215aeb7912e61d52cd2925eefe18b
Bryan Drewery <bryan@shatow.net>
2025-03-09 06:24:03 -0700

    update: Be quiet during gc

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ update() {
 	git submodule update --init --depth=1
 	git reset --hard origin/master
 	git reflog expire --expire-unreachable=all --all
-	git gc --prune=all
+	git gc --prune=all --quiet
 	exec ./install.sh
 }
 update
