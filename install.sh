f6748fa486f9785d73707287e9fe1c0670d0b6fa
Bryan Drewery <bryan@shatow.net>
2025-05-08 07:09:52 -0700

    k -> script

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ fi
 for f in alert-on-exit.sh benv.sh generate-tags generate-tagsd \
     screen-wrapper.sh ssh-wrapper.sh fixscreen make start-screen tf update-profile \
     fping-wait.sh \
-    gen-ssh \
+    gen-ssh k \
     lscolors lscolors 24-bit-color.sh; do
 	if [ -L ~/bin/${f} ]; then
 		rm -f ~/bin/${f}
