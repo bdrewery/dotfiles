79dc5fc94346b0d6a6e03d44b5113be9427243fb
Bryan Drewery <bryan@shatow.net>
2026-05-06 13:34:14 -0700

    update.sh: Need to reset to master before submodule update

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@
 update() {
 	cd ~/.profile-repo
 	git fetch --no-recurse-submodules origin --depth=1
-	git submodule update --init --depth=1
 	git reset --hard origin/master
+	git submodule update --init --depth=1
 	git reflog expire --expire-unreachable=all --all
 	git gc --prune=all --quiet
 	exec ./install.sh
