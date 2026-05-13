50e6c8713b5a48b61b86449d6886b4cd8b98cdc4
Bryan Drewery <bryan@shatow.net>
2026-05-13 13:51:04 -0700

    update: Be quiet

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 update() {
 	cd ~/.profile-repo
-	git fetch --no-recurse-submodules origin --depth=1
+	git fetch -q --no-recurse-submodules origin --depth=1
 	git reset --hard origin/master
-	git submodule update --init --depth=1
+	git submodule -q update --init --depth=1
 	git reflog expire --expire-unreachable=all --all
 	git gc --prune=all --quiet
 	exec ./install.sh
