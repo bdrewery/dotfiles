bdrewery/dotfiles

commit 1197f8fb019fae1af191562529a5f550e5942692

fping-wait: Avoid looping on DNS failures

bin/fping-wait.sh
@@ -83,6 +83,20 @@ backoff() {
 	fi
 }
 
+fping() {
+	local ret
+
+	ret=0
+	command fping "$@" || ret="$?"
+	case "${ret}" in
+	2)
+		# Some error like DNS failure.
+		sleep 1
+		;;
+	esac
+	return "${ret}"
+}
+
 while :; do
 	if ! fping -r 0 "${hostname}" ||
 	   ! checkport "${hostname}" "${port}"; then

commit 3206d062c13cac3556566e8f481314b0e08981cd

fping-wait: Fix not using timeout for fping.
Fixes: 1197f8fb

bin/fping-wait.sh
@@ -6,13 +6,11 @@ fping() {
 	ret=0
 	command timeout --preserve-status 5 fping -q "$@" || ret="$?"
 	case "${ret}" in
-	2|3)
-		error="$(command fping "$@" 2>&1 || :)"
-		echo "[!] fping: ${error}" >&2
+	2)
+		# Some error like DNS failure.
+		echo "[!] fping error" >&2
 		backoff
 		;;
-	esac
-	case "${ret}" in
 	3)
 		# Invalid arguments
 		exit 1
@@ -81,20 +79,6 @@ backoff() {
 		#exit 1
 		PERIOD=1
 	fi
-}
-
-fping() {
-	local ret
-
-	ret=0
-	command fping "$@" || ret="$?"
-	case "${ret}" in
-	2)
-		# Some error like DNS failure.
-		sleep 1
-		;;
-	esac
-	return "${ret}"
 }
 
 while :; do